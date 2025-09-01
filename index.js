49356aabdc8f7176cfc5dc39ac2e5c5e4317d521
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2025-09-01 09:59:17 -0300

    Create function to get computer choice

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+console.log(`Computer choice: ${getComputerChoice()}.`);
+
+function getComputerChoice() {
+    const choiceCount = 3;
+    const choiceValue = Math.random() * choiceCount;
+    
+    if (choiceValue <= 1) {
+        return "rock";
+    }
+
+    if (choiceValue <= 2) {
+        return "paper";
+    }
+
+    return "scissors";
+}
