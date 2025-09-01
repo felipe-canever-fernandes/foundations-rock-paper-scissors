fe37887fc1bec26f4c7be60b0adda5d899ab4c07
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2025-09-01 10:56:36 -0300

    Indent using tabs

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ console.log(`Human choice: ${getHumanChoice()}.`);
 console.log(`Computer choice: ${getComputerChoice()}.`);
 
 function getHumanChoice() {
-    const choice = prompt("Rock, paper, or scissors?");
-    const lowercaseChoice = choice.toLowerCase();
-    return lowercaseChoice;
+	const choice = prompt("Rock, paper, or scissors?");
+	const lowercaseChoice = choice.toLowerCase();
+	return lowercaseChoice;
 }
 
 function getComputerChoice() {
-    const choiceCount = 3;
-    const choiceValue = Math.random() * choiceCount;
-    
-    if (choiceValue <= 1) {
-        return "rock";
-    }
+	const choiceCount = 3;
+	const choiceValue = Math.random() * choiceCount;
+	
+	if (choiceValue <= 1) {
+		return "rock";
+	}
 
-    if (choiceValue <= 2) {
-        return "paper";
-    }
+	if (choiceValue <= 2) {
+		return "paper";
+	}
 
-    return "scissors";
+	return "scissors";
 }
